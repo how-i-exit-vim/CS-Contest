7d7a782af0a79a89e719446e7b1e874f9941241a
Kugge <sofiane.djerbi38@gmail.com>
2021-11-08 11:03:53 +0100

    Cat problem

---
+++ cats/main.py
@@ -0,0 +1,61 @@
+import sys
+
+def solve(r, c):
+   matrix = [[0]*len(c) for _ in range(len(r))]
+   visited = set()
+
+   def minimum(r,c):
+      min_total = float('inf')
+   
+      type = ''
+      for i in range(len(r)):
+         if(r[i] < min_total):
+            index = i
+            type = 'row'
+            min_total = r[i]
+
+      for i in range(len(c)):
+         if(c[i] < min_total):
+            min_total = c[i]
+            type = 'col'
+            index = i
+
+      if(type == 'row'):
+         r[index] = float('inf')
+
+         for i in range(len(c)):
+            if(c[i] != float('inf') and c[i] >= min_total):
+               c[i] -= min_total
+               matrix[index][i] = min_total
+               break
+
+      if(type == 'col'):
+         c[index] = float('inf')
+         for i in range(len(r)):
+            if(r[i] != float('inf') and r[i] >= min_total):
+               r[i] -= min_total
+               matrix[i][index] = min_total
+               break
+
+      visited.add((index,type))
+
+   while len(visited) != len(r)+len(c):
+      minimum(r,c)
+
+   return matrix
+
+ms = []
+for a in sys.stdin:
+    m = [int(c) for c in a.split()]
+    ms.append(m)
+
+rowSum = ms[0]
+colSum = ms[1]
+
+x = solve(rowSum, colSum)
+for i in x:
+    for j in range(len(i)):
+        print(i[j], end="")
+        if j != len(i)-1:
+            print(" ", end="")
+    print("")
