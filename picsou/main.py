a1ac100fc95cfe6137cd59deb90111b9fd9748cd
Kugge <sofiane.djerbi38@gmail.com>
2021-11-08 11:49:59 +0100

    picsou problem

---
+++ picsou/main.py
@@ -0,0 +1,27 @@
+import sys
+
+nb = 0
+c = 0
+ms = []
+for a in sys.stdin:
+    a = a[:-1]
+    d = ""
+    if "." in a:
+        n, d = a.split(".")
+        c = max(c, len(d))
+        a = n+d
+    ms.append((a, len(d)))
+
+for (i,d) in ms:
+    i = i + "0"*(c-d)
+    nb += int(i)
+
+nb = str(nb)
+nb = nb[:-c] + '.' + nb[-c:]
+nb = nb.strip("0")
+nb = nb.strip(".")
+if nb == "":
+    nb = "0"
+nb = nb.strip(" ")
+print(nb)
+
