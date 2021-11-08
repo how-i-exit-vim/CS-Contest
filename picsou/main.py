3acf7b762858795a4d97802b47e32eb39dd6f082
Kugge <sofiane.djerbi38@gmail.com>
2021-11-08 15:36:25 +0100

    fixed 5 + 5 = 1

--- picsou/main.py
+++ picsou/main.py
@@ -18,11 +18,12 @@ for (i,d) in ms:
     nb += int(i)
 
 nb = str(nb)
-n, d = nb[:-c], nb[-c:]
-n = n.lstrip("0")
-d = d.rstrip("0")
-nb = n + '.' + d
-nb = nb.strip(".")
+if c != 0:
+    n, d = nb[:-c], nb[-c:]
+    n = n.lstrip("0")
+    d = d.rstrip("0")
+    nb = n + '.' + d
+    nb = nb.strip(".")
 
 if nb == "":
     nb = "0"
